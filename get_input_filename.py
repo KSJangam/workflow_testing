e251f2b7b10e2b948e95e7f6ee1cf4a0c61eb5c7
Kunal Jangam <kjangam8@gmail.com>
2022-06-19 16:07:03 -0400

    take ??

--- get_input_filename.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-import os
-
-env_file = os.getenv('GITHUB_ENV')
-with open(env_file, "a") as myfile:
-    myfile.write("inputfile=input/"+os.listdir("input")[0])
