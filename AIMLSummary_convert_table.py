0151e7c3f1f1039d899b2f1313089aa696c9d578
Kunal Jangam <kjangam8@gmail.com>
2022-06-19 15:35:14 -0400

    take ??

--- AIMLSummary_convert_table.py
+++ AIMLSummary_convert_table.py
@@ -12,7 +12,7 @@ parser=argparse.ArgumentParser(description="io files")
 parser.add_argument("outputfile", type=argparse.FileType('w'), help="output file")
 args=parser.parse_args()
 x="input/"+os.listdir("input")[0]
-df1=pandas.read_csv(x, keep_default_na=False)
+df1=pandas.read_csv(x, encoding= 'unicode_escape', keep_default_na=False)
 
 nodes = dict() #create dictionary to store the ID of classification nodes
 i = 1 #keep a counter to assign node ID
