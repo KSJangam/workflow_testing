149f8a6525651be7db7db9736e4b5143eb239102
Kunal Jangam <kjangam8@gmail.com>
2022-06-19 16:54:07 -0400

    take ??

--- get_input_filename.py
+++ get_input_filename.py
@@ -3,4 +3,4 @@ import os
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as myfile:
-    myfile.write("inputfile=input/"+os.listdir("input")[0])
+    myfile.write("inputfile=input/"+os.listdir("input")[1])
