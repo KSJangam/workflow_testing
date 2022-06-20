186523abc0a9da77e2aaee1c368735b1ef8be366
Kunal Jangam <kjangam8@gmail.com>
2022-06-20 15:08:08 -0400

    take ???

--- get_input_filename.py
+++ get_input_filename.py
@@ -1,6 +1,16 @@
 import os
 
+import argparse
+#import os 
+parser=argparse.ArgumentParser(description="io files")
+
+parser.add_argument("newfile",  help="newfile")
+
+args=parser.parse_args()
+print('hi')
+print(args.newfile)
+print('bye')
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as myfile:
-    myfile.write("inputfile=input/"+os.listdir("input")[-1])
+    myfile.write(args.newfile[-1])
