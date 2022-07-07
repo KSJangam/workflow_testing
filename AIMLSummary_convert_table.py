9bbcd0a629bb85655e5689d18c3f3bad373ba627
Kunal Jangam <kjangam8@gmail.com>
2022-07-07 14:23:59 -0400

    json output

--- AIMLSummary_convert_table.py
+++ AIMLSummary_convert_table.py
@@ -4,62 +4,73 @@
 
 import pandas
 import argparse
-#import os 
+from tinydb import TinyDB, Query
+import os
 parser=argparse.ArgumentParser(description="io files")
 
 parser.add_argument("inputfile", type=argparse.FileType('r'), help="input file")
 
 parser.add_argument("outputfile", type=argparse.FileType('w'), help="output file")
 args=parser.parse_args()
-#x="input/"+os.listdir("input")[0]
 df1=pandas.read_csv(args.inputfile, encoding='UTF-8', keep_default_na=False)
 
-nodes = dict() #create dictionary to store the ID of classification nodes
-i = 1 #keep a counter to assign node ID
-
-
-
-cols=[list() for num in range(3)]
-
+if os.path.exists(args.outputfile):
+    os.remove(args.outputfile)
+tdb=TinyDB(args.outputfile)
+q=Query()
 
 col_names=df1.columns
 
 
+tdb.insert({'name':'Classification', 'parent':"null", 'children':[]})
+
+def parse_row(row, index, parent, l):
+    if len(col_names)>index and row[col_names[index]]=="":
+        return parse_row(row, index+1, parent, l)
+    
+    if len(col_names)>index+1:
+        item = [x for x in l if x['name']==row[col_names[index]]]
+        if len(item)==0:
+            children=[parse_row(row, index+1, row[col_names[index]], [])]
+            if children == [""]:
+                print("here")
+                return {'name':row[col_names[index]], 'parent':parent}
+            else:
+                return {'name':row[col_names[index]], 'parent':parent, 'children':children}
+        else:
+            children=item[0]['children']
+            new_item=parse_row(row, index+1, row[col_names[index]], children)
+            if new_item != "":
+                added=False
+                for num in range(len(children)):
+                    if children[num]['name']==new_item['name']:
+                        children[num]=new_item
+                if not added:
+                    children.append(new_item)
+            return {'name':row[col_names[index]], 'parent':parent, 'children':children}
+    elif len(col_names)==index+1:
+        if row[col_names[index]]=="":
+            return ""
+        else:
+            return {'name':row[col_names[index]], 'parent':parent}
+    
+    return ""
+            
+
 df1 = df1.reset_index()  # make sure indexes pair with number of rows
 for index, row in df1.iterrows():
-    if row[col_names[0]]+str(0) not in nodes.keys() and row[col_names[0]]!="":
-        nodes[row[col_names[0]]+str(0)]=[i, [0]]
-        cols[0].append(0)
-        cols[1].append(i)
-        cols[2].append(row[col_names[0]])
-        i=i+1
-    for num in range(1, len(col_names)):
-        if row[col_names[num]]!="": 
-            backstep=1
-            while num-backstep>=0 and row[col_names[num-backstep]]=="":
-                backstep=backstep+1
-            if num-backstep<0:
-                p=0
-            else:
-                p=nodes[row[col_names[num-backstep]]+str(num-backstep)][0]
-            if row[col_names[num]]+str(num) not in nodes.keys(): 
-                nodes[row[col_names[num]]+str(num)]=[i, [p]]  
-                cols[0].append(p)
-                cols[1].append(i)
-                cols[2].append(row[col_names[num]])
-                i=i+1
-            elif p not in nodes[row[col_names[num]]+str(num)][1]:
-                nodes[row[col_names[num]]+str(num)][1].append(p)
-                cols[0].append(p)
-                cols[1].append(nodes[row[col_names[num]]+str(num)][0])
-                cols[2].append(row[col_names[num]])
-
-
-d = {'ParentID':cols[0], 'ChildID':cols[1], 'Name':cols[2]}
-df = pandas.DataFrame(data = d)
-
-df.to_csv(args.outputfile, encoding = "utf-8",index=False)
-
+    root=tdb.all()[0]
+    i=0
+    while(row[col_names[i]]==""):
+        i=i+1  
+    new_item = parse_row(row, i, root['name'], root['children'])
+    added=False
+    for num in range(len(root['children'])):
+        if root['children'][num]['name']==new_item['name']:
+            root['children'][num]=new_item
+    if not added:
+        root['children'].append(new_item)
+    tdb.update({'children':root['children']}, q.name=="Classification")
 
 
 
