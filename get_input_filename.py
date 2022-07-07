3c92751669b3c8ccf7cbefce1e1195265bdbd5cd
Kunal Jangam <kjangam8@gmail.com>
2022-07-07 15:36:59 -0400

    cleanup

--- get_input_filename.py
+++ get_input_filename.py
@@ -7,9 +7,6 @@ parser=argparse.ArgumentParser(description="io files")
 parser.add_argument("newfile",  help="newfile")
 
 args=parser.parse_args()
-print('hi')
-print(args.newfile)
-print('bye')
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as myfile:
