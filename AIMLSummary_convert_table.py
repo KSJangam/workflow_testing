6fb80ed4cf5341b87969bedaa585c1b46d8bf7cb
Kunal Jangam <kjangam8@gmail.com>
2022-07-08 11:06:49 -0400

    no more tinydb

--- AIMLSummary_convert_table.py
+++ AIMLSummary_convert_table.py
@@ -4,23 +4,19 @@
 
 import pandas
 import argparse
-from tinydb import TinyDB, Query
+import json
+
+
 parser=argparse.ArgumentParser(description="io files")
-
 parser.add_argument("inputfile", type=argparse.FileType('r'), help="input file")
-
 parser.add_argument("outputfile", type=str, help="output file")
-
 args=parser.parse_args()
-df1=pandas.read_csv(args.inputfile, encoding='UTF-8', keep_default_na=False)
 
-tdb=TinyDB(args.outputfile)
-q=Query()
-tdb.remove(q.name=='Classification')
+df1=pandas.read_csv(args.inputfile, encoding='UTF-8', keep_default_na=False)
 
 col_names=df1.columns
 
-tdb.insert({'name':'Classification', 'parent':"null", 'children':[]})
+data = {'name':'Classification', 'parent':"null", 'children':[]}
 
 def parse_row(row, index, parent, l):
     if len(col_names)>index and row[col_names[index]]=="":
@@ -42,6 +38,7 @@ def parse_row(row, index, parent, l):
                 for num in range(len(children)):
                     if children[num]['name']==new_item['name']:
                         children[num]=new_item
+                        added=True
                 if not added:
                     children.append(new_item)
             return {'name':row[col_names[index]], 'parent':parent, 'children':children}
@@ -54,20 +51,20 @@ def parse_row(row, index, parent, l):
     return ""
             
 
-df1 = df1.reset_index()  # make sure indexes pair with number of rows
+df1 = df1.reset_index()  
 for index, row in df1.iterrows():
-    root=tdb.all()[0]
     i=0
     while(row[col_names[i]]==""):
         i=i+1  
-    new_item = parse_row(row, i, root['name'], root['children'])
+    new_item = parse_row(row, i, data['name'], data['children'])
     added=False
-    for num in range(len(root['children'])):
-        if root['children'][num]['name']==new_item['name']:
-            root['children'][num]=new_item
+    for num in range(len(data['children'])):
+        if data['children'][num]['name']==new_item['name']:
+            data['children'][num]=new_item
+            added=True
     if not added:
-        root['children'].append(new_item)
-    tdb.update({'children':root['children']}, q.name=="Classification")
+        data['children'].append(new_item)
 
 
-
+with open(args.outputfile, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
